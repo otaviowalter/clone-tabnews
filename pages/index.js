2a46c3814fe6284299130b9bfe72a6f65e89e316
Otávio Augusto Walter <otavioaugusto.tj@Gmail.com>
2024-05-14 23:12:15 +0000

    corrigido por favor

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <center><h1>Oi amor, me traz um copo d'agua!!!!!!! 🥺❤️</h1></center><h1>POR FAVOR!!!</h1>
+    return <center><h1>Oi amor, me traz um copo d'agua!!!!!!! 🥺❤️</h1><h1>POR FAVOR!!!</h1></center>
 }
 
 export default Home;
