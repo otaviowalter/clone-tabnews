817bd69ace607c8c2f582f9be0d1485dc01b5c37
Otávio Augusto Walter <otavioaugusto.tj@Gmail.com>
2024-05-14 23:03:38 +0000

    adicionado exclamação

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <center><h1>Oi amor, me traz um copo d'agua 🥺❤️</h1></center>
+    return <center><h1>Oi amor, me traz um copo d'agua!!!!!!! 🥺❤️</h1></center>
 }
 
 export default Home;
